bfb40e6482094a65330bc653d8289dfd0112fbd5
Carl Vuorinen <carl.vuorinen@gmail.com>
2020-01-02 18:23:51 +0200

    Check party members hp on boss damage
    Send Discord message with @Healer mention when ever at least 3 members have below 25 hp

--- src/functions/habicord.js
+++ src/functions/habicord.js
@@ -68,8 +68,13 @@ exports.handler = async function(event) {
           parseFloat(params.chat.info.bossDamage) > 20
         ) {
           const attackMessage = habiticaMessage.match(/\. (.+\.)`?$/i);
-          discordMessage = `Help, we are taking a beating here!
+          discordMessage = `Send help, we are taking a beating here!
 > ${attackMessage.length ? attackMessage[1] : habiticaMessage}`;
+        } else {
+          const lowHpCount = await checkPartyHealth();
+          if (lowHpCount > 2) {
+            discordMessage = `Alert the @Healer! Party needs some :heart:`;
+          }
         }
         break;
     }
@@ -108,4 +113,43 @@ async function sendDiscordMessage(content) {
       `${response.status} ${response.statusText} "${await response.text()}"`
     );
   }
+}
+
+async function checkPartyHealth(lowHpLimit = 25) {
+  const members = await fetchPartyMembers();
+
+  return members.map(member => member.stats.hp).filter(hp => hp < lowHpLimit)
+    .length;
+}
+
+async function fetchPartyMembers() {
+  const url =
+    process.env.HABITICA_API_URL +
+    "/groups/party/members?includeAllPublicFields=true";
+  const options = {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      // X-Client header as per instructions here: https://habitica.fandom.com/wiki/Guidance_for_Comrades#X-Client_Header
+      "x-client": process.env.HABITICA_X_CLIENT,
+      "x-api-user": process.env.HABITICA_USER_ID,
+      "x-api-key": process.env.HABITICA_API_KEY
+    }
+  };
+
+  return fetchJson(url, options).then(response => response.data);
+}
+
+async function fetchJson(url, options) {
+  //console.log("fetch", { url, options });
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw Error(
+      `${response.status} ${response.statusText} "${await response.text()}"`
+    );
+  }
+
+  return response.json();
 }
